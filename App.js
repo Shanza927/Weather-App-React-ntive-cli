2180859ce52f5e7d8351078051f64c5511cf9292
shanza amin <shanzaamin530@gmail.com>
2021-08-30 03:56:23 -0700

    Navigator

--- App.js
+++ App.js
@@ -24,14 +24,80 @@ import Home from './screens/Home.js';
 
 
 
-
+const Tab = createBottomTabNavigator();
 const App = () => {
  
 
   return (
     <View>
     <StatusBar backgroundColor = "#00aaff"  barStyle = "dark-content"   />
-    <Home/>
+     <NavigationContainer>
+            <Tab.Navigator
+      initialRouteName="home"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="search"
+        component={Search}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+          tabBarBadge: 3,
+        }}
+      />
+      
+    </Tab.Navigator>
+    </NavigationContainer>
+    
+    {/* <NavigationContainer>
+     <Tab.Navigator
+     initialRouteName="home"
+      screenOptions={({route})=>({
+        tabBarIcon:({color})=> {
+          let iconName ;
+          if(route.name==="home"){
+            iconName="home" 
+          }
+          else if (route.name==="search"){
+            iconName="search-plus" ;
+          }
+          return <MaterialCommunityIcons name ={iconName} size ={20} color={color} />
+        }
+      })}
+      tabBarOptions ={{
+        activeTintColor:"white",
+        inactiveTintColor:"gray"
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={Home}
+        
+      />
+      <Tab.Screen
+        name="search"
+        component={Search}
+       
+      />
+      
+    </Tab.Navigator>
+
+    </NavigationContainer> */}
+ 
     </View>
     
   );
